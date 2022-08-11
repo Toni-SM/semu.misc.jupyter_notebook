65111870df54d9e7d72d93930255e8116ebe510b
Antonio Serrano Muñoz <toni.semu@gmail.com>
2022-08-11 22:45:43 +0000

    Use the Omniverse application's python executable

--- exts/semu.misc.jupyter_notebook/data/provisioners/embedded_omniverse_python3/provisioner_socket.py
+++ exts/semu.misc.jupyter_notebook/data/provisioners/embedded_omniverse_python3/provisioner_socket.py
@@ -10,8 +10,10 @@ from jupyter_client.launcher import launch_kernel
 class Provisioner(LocalProvisioner):
     async def launch_kernel(self, cmd: List[str], **kwargs: Any) -> KernelConnectionInfo:
         # set paths
-        cmd[0] = "/isaac-sim/kit/python/bin/python3" # TODO: replace with the path to the Omniverse python3 
+        cmd[0] = os.path.abspath(os.path.join(os.path.dirname(os.__file__), "..", "..", "bin", "python3"))
         cmd[1] = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "launchers", "ipykernel_launcher.py"))
+
+        self.log.info("Launching kernel: %s", " ".join(cmd))
 
         scrubbed_kwargs = LocalProvisioner._scrub_kwargs(kwargs)
         self.process = launch_kernel(cmd, **scrubbed_kwargs)
